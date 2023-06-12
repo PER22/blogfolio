a97a5acb8d39efcaab2993694404a45a210a327f
Padraic Reilly <patrick.reilly22@gmail.com>
2023-06-12 09:26:14 -0700

    Last touches

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,4 +1,6 @@
 export default function HomePage(){
   return <h1>Home Page</h1>;
-  <p>BlogFolio exists to help you link written material to your deployed code projects. Create a project, and then write blog posts about it as you work towards completing it. Share your thought process, and log your progress.</p>
+  <p>BlogFolio exists to help you link written material to your deployed code projects. 
+    Create a project, and then write blog posts about it as you work towards completing it. 
+    Share your thought process, and log your progress.</p>
 };
