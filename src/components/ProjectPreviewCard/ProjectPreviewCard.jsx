9324d0156577e41e47b1f892fdf671a5a56a3d6a
Padraic Reilly <patrick.reilly22@gmail.com>
2023-07-05 12:52:42 -0700

    Revert to local development package.json

--- src/components/ProjectPreviewCard/ProjectPreviewCard.jsx
+++ src/components/ProjectPreviewCard/ProjectPreviewCard.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export default function ProjectPreviewCard({ project }) {
     return (<>
         <div className='info-card' key={project._id}>
-            <Link to={`/projects/${project._id}`}><h2 >{project.title}</h2></Link>
+            <Link to={`/projects/${project._id}`}><h2>{project.title}</h2></Link>
             {project.image && <img className="project-image" src={project.image} alt={project.title} />}
             <ReactMarkdown className="project-description-paragraph">{`${project.description.split(' ').slice(0, 100).join(' ')}...`}</ReactMarkdown>
         </div>
